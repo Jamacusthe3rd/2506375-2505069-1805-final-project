a0db4b8bb4a39a269c2c274caee3ea55e5a86fd9
JamesStimpsonRHUL <znwf282@live.rhul.ac.uk>
2025-02-17 11:46:41 +0000

    tile mapping

--- sketch.js
+++ sketch.js
@@ -3,19 +3,53 @@ let tileMap = []
 let tilesX = 10
 let tilesY = 10
 let tileSize = 50
+let textures = []
 
+let graphicMap = [
+//    THESE ARE OUR Y VALUES
+// 0  1  2  3  4  5  6  7  8  9
+	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 0
+	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 1
+	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 2
+	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 3
+  [2, 2, 2, 2, 0, 0, 0, 1, 1, 1], // 4
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 5   THESE ARE OUR X VALUES
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 6
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 7
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 8
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]  // 9
+]
 
-
+let tileRules = [
+  //    THESE ARE OUR Y VALUES
+  // 0  1  2  3  4  5  6  7  8  9
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 0
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 1
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 2
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 3
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 4
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 5   THESE ARE OUR X VALUES
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 6
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 7
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 8
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]  // 9
+  ]
 
 
 class Tile { //creates class for a tile
-  constructor(tileX,tileY,tileSize,tileID) {
+  constructor(texture, tileX, tileY, tileSize, tileID) {
+    this.texture = texture; //texture of tile in tileMap grid
     this.tileX = tileX; //x position of tile in tileMap grid
     this.tileY = tileY; //y position of tile in tileMap grid
     this.tileSize = tileSize; //size of tiles in pixels
     this.xPos = this.tileX * this.tileSize; //x pixel position of tile in canvas
     this.yPos = this.tileY * this.tileSize; //y pixel position of tile in canvas
     this.tileID = tileID;
+  }
+
+  display() {
+    noStroke()
+    image(this.texture, this.xPos, this.yPos, tileSize, tileSize)
   }
 
   debugGrid() { //used to display the x and y of the tiles in the tileMap scale (not default pixel scale), along with the tiles IDs
@@ -60,9 +94,10 @@ class Tile { //creates class for a tile
 }
 
 function preload() {
-  tile_grass = loadImage('sprites/tile_grass.png')
-  tile_water = loadImage('sprites/tile_water.png')
-
+  textures[0] = loadImage('sprites/tile_grass.png')
+  textures[1] = loadImage('sprites/tile_water.png')
+  textures[2] = loadImage('sprites/tile_wood.png')
+  textures[3] = loadImage('sprites/tile_stone.png')
 }
 
 function setup() {
@@ -71,7 +106,8 @@ function setup() {
   for (let tileX = 0; tileX < tilesX; tileX++) {
     tileMap[tileX] = []
     for (let tileY = 0; tileY < tilesY; tileY++) {
-      tileMap[tileX][tileY] = new Tile(tileX, tileY, tileSize, tileID) //creates a tile object for every 50 pixels down and every 50 pixels right
+      let texture = graphicMap[tileY][tileX]
+      tileMap[tileX][tileY] = new Tile(textures[texture], tileX, tileY, tileSize, tileID) //creates a tile object for every 50 pixels down and every 50 pixels right
       tileID++
     }
   }
@@ -81,12 +117,11 @@ function draw() {
   background(0);
   for (let tileX = 0; tileX < tilesX; tileX++) {
     for (let tileY = 0; tileY < tilesY; tileY++) {
-      tileMap[tileX][tileY].debugGrid() //displays the x and y of the tile in the tileMap scale (not default pixel scale), along with the tile ID
+      tileMap[tileX][tileY].display()
+      //tileMap[tileX][tileY].debugGrid() //displays the x and y of the tile in the tileMap scale (not default pixel scale), along with the tile ID
+      
     }
   }
 
-  tileMap[5][6].displayMessage()//adding a message to specified tiles
-  tileMap[0][8].displayMessage()
-  tileMap[3][4].displayMessage()
-  tileMap[3][0].displayMessage()
+  //tileMap[5][6].displayMessage()//adding a message to specified tiles
 }
