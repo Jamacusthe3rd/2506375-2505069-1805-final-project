49c54bdeb9c3d6ff4d00aae2bfd9d9ae31e099f8
JamesStimpsonRHUL <znwf282@live.rhul.ac.uk>
2025-04-04 12:27:51 +0100

    tile map landscape

--- sketch.js
+++ sketch.js
@@ -1,42 +1,60 @@
 //setting up initial values
 let tileMap = []
-let tilesX = 10
-let tilesY = 10
+let tilesX = 32
+let tilesY = 16
 let tileSize = 50
 let textures = []
 let player1 
 let leftOrRightLeg = true
 let leg = 1
 
+let directionState = 0
+let up = 0
+let left = 1
+let down = 2
+let right = 3
+
 let graphicMap = [
 //    THESE ARE OUR Y VALUES
-// 0  1  2  3  4  5  6  7  8  9
-	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 0
-	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 1
-	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 2
-	[3, 3, 3, 2, 0, 0, 0, 1, 1, 1], // 3
-  [2, 2, 2, 2, 0, 0, 0, 1, 1, 1], // 4
-  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 5   THESE ARE OUR X VALUES
-  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 6
-  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 7
-  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 8
-  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]  // 9
+// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+	[0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1], // 0
+	[0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1], // 1
+	[0, 0, 0, 0, 0, 0, 1, 1, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1], // 2
+	[0, 0, 0, 0, 0, 0, 1, 1, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1], // 3
+  [0, 0, 0, 0, 0, 0, 1, 1, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 1, 1, 1, 1, 1, 1, 1], // 4
+  [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1], // 5   THESE ARE OUR X VALUES
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 6
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 7
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], // 8
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 9
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 10
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 11
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 12
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 13
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 14
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]  // 15
 ]
 
 let tileRules = [
-  //    THESE ARE OUR Y VALUES
-  // 0  1  2  3  4  5  6  7  8  9
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 0
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 1
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 2
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 3
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 4
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 5   THESE ARE OUR X VALUES
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 6
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 7
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1], // 8
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]  // 9
-  ]
+//    THESE ARE OUR Y VALUES
+// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 0
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 1
+[0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1], // 2
+[0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1], // 3
+[0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1], // 4
+[0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1], // 5   THESE ARE OUR X VALUES
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 6
+[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // 7
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], // 8
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 9
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 10
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 11
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 12
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 13
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], // 14
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]  // 15
+]
 
 
 class Tile { //creates class for a tile
@@ -48,7 +66,6 @@ class Tile { //creates class for a tile
     this.xPos = this.tileX * this.tileSize; //x pixel position of tile in canvas
     this.yPos = this.tileY * this.tileSize; //y pixel position of tile in canvas
     this.tileID = tileID;
-    this.isMoving = false;
   }
 
   display() {
@@ -101,11 +118,12 @@ class Player {
   constructor(currentSpriteGroup, startX, startY, tileRules){
     this.currentSpriteGroup = currentSpriteGroup
     this.currentSprite = this.currentSpriteGroup[0]
-    this.tileX = startX
-    this.tileY = startY
-    this.speed = 0.2
+    this.posX = startX
+    this.posY = startY
+    this.speed = 0.1
     this.tileSize = tileSize
     this.tileRules = tileRules
+    this.isMoving = false
 
   }
 
@@ -117,45 +135,52 @@ class Player {
       this.isMoving = false
     }
     if (keyIsDown(87) == true){
+      directionState = up
       this.currentSpriteGroup = sprites.knight_up
-      this.tileY -= this.speed
+      this.posY -= this.speed
     }
-    else{
-      if (keyIsDown(65) == true){
+    else if (keyIsDown(65) == true){
+      directionState = left
       this.currentSpriteGroup = sprites.knight_left
-      this.tileX -= this.speed
+      this.posX -= this.speed
     }
+    else if (keyIsDown(83) == true){
+      directionState = down
+      this.currentSpriteGroup = sprites.knight_down
+      this.posY += this.speed
+    }
+    else if (keyIsDown(68) == true){
+      directionState = right
+      this.currentSpriteGroup = sprites.knight_right
+      this.posX += this.speed
+    }
+      
+    
+    if (this.isMoving){
+      setInterval(function() {leftOrRightLeg = !leftOrRightLeg}, 1000)
+      if (leftOrRightLeg){
+        leg = 1
+      }
       else{
-        if (keyIsDown(83) == true){
-        this.currentSpriteGroup = sprites.knight_down
-        this.tileY += this.speed
+        leg = 2
       }
-        else{
-          if (keyIsDown(68) == true){
-          this.currentSpriteGroup = sprites.knight_right
-          this.tileX += this.speed
-        }
-      }
-    }
-  }
-  if (this.isMoving){
-    setInterval(function () {leftOrRightLeg = !leftOrRightLeg}, 1000)
-    if (leftOrRightLeg){
-      leg = 1
+      this.currentSprite = this.currentSpriteGroup[leg]
     }
     else{
-      leg = 2
+      this.currentSprite = this.currentSpriteGroup[0]
     }
-    this.currentSprite = this.currentSpriteGroup[leg]
   }
-  else{
-    this.currentSprite = this.currentSpriteGroup[0]
-  }
-}
+
+  
+  // attack (){
+  //   if (keyIsPressed(32)){
+
+  //   }
+  // }
 
   display (){
     //player sprite     //player x  //player y   //width   //height
-    image(this.currentSprite,this.tileX,this.tileY,tileSize,tileSize*2)
+    image(this.currentSprite,this.posX,this.posY,tileSize,tileSize*2)
   }
 
 }
@@ -177,7 +202,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(1600,800);
   let tileID = 0
   for (let tileX = 0; tileX < tilesX; tileX++) {
     tileMap[tileX] = []
